773269ec4f9311299f4a3e11467f40ebd73fcd2d
Konstantin Suhov <konstantin.suhov@gmail.com>
2017-02-12 15:23:28 +0300

    Coockbock created

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'aws-sj'
+maintainer       'ScienceJet'
+maintainer_email 'ks@sciencejet.net'
+license          'All rights reserved'
+description      'Installs/Configures aws-sj'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
